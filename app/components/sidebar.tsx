e4ee899fdba32f18519a56d560dbe49b23a932ff
Charles Cai <caicongchen@gmail.com>
2024-07-30 20:25:38 -0400

    rename a few things to CharlesC.AI

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -223,7 +223,7 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
+        title="CharlsC.AI"
         subTitle="Build your own AI assistant."
         logo={<ChatGptIcon />}
       >
